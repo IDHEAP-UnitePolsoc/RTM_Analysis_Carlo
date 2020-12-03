b053a2d2977b7afdf8b41dbf101154f1536c1edf
cknotz <carlo.knotz@gmail.com>
2020-12-03 20:52:13 +0100

    Removed code for correlation coefficients (rho/effects.xlsx)!!!!

--- oecd_rtm_macro.R
+++ oecd_rtm_macro.R
@@ -44,53 +44,9 @@ unem <- as.data.frame(readSDMX(url)) %>%
     dplyr::rename(country=COUNTRY,yunem=obsValue)
 
 
-# Simple correlation coefficients
-#################################
-effects <- readxl::read_excel("cntryeffects.xlsx")
 
 
-# Graph results - rhos
-effects %>% 
-    #mutate(cntry = factor(effects$Country, levels = effects$Country[order(effects$rho_passive, decreasing = T)])) %>% 
-    dplyr::select(Country,rho_passive) %>% 
-    #pivot_longer(cols = c("rho_active","rho_passive"),
-     #            names_to="inds",
-      #           values_to="vals") %>% 
-    ggplot(aes(x=reorder(Country, -rho_passive),y=rho_passive)) +
-        geom_bar(position = "dodge", stat = "identity") +
-        geom_rect(aes(ymin=-.4,ymax=.4,xmin=-Inf,xmax=Inf), alpha=0.05,fill="pink") +
-        ylab(paste0("Correlation coefficient betw.\n RTI & support for passive measures")) +
-        xlab(" ")
-    ggsave("rti_passive_over.pdf",
-           width = 8,
-           height = 5)
-    
-
-effects %>% 
-    dplyr::select(Country, rho_active) %>% 
-    ggplot(aes(x=reorder(Country, -rho_active),y=rho_active)) +
-        geom_bar(position = "dodge", stat = "identity")  +
-        geom_rect(aes(ymin=-.4,ymax=.4,xmin=-Inf,xmax=Inf), alpha=0.05,fill="pink") +
-        ylab(paste0("Correlation coefficient betw.\n RTI & support for passive measures")) +
-        xlab(" ")
-      ggsave("rti_active_over.pdf",
-           width = 8,
-           height = 5)
-
-
-effects %>%
-    dplyr::select(Country,rho_passive, rho_active) %>% 
-    ggplot(aes(x=rho_passive,y=rho_active)) +
-        geom_rect(aes(xmin=-0.3,ymin=-Inf,ymax=Inf,xmax=.3), alpha=0.01,fill="pink") +
-        geom_rect(aes(xmin=-Inf,ymin=-0.3,ymax=0.3,xmax=Inf), alpha=0.01,fill="green") +
-        geom_point() +
-        geom_text(aes(label=Country)) +
-        geom_hline(yintercept = 0) +
-        geom_vline(xintercept = 0)
-
-# Reading in OECD Educational attainment data
-#############################################
-
+# OECD Educational attainment data
 url <- "https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/EAG_NEAC/AUS+AUT+BEL+CAN+CHL+COL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA.L3T4+L3+L3_C5+L4+L4_C5+L3T4_C4+L3T4_C5+L3T4_X+L5T8.T+F+M.Y25T64+Y25T34.T.VALUE.NEAC_SHARE_EA/all?startTime=9999&endTime=9999"
 
 edudata <- as.data.frame(readSDMX(url)) %>% 
@@ -116,27 +72,6 @@ isced <- edudata %>%
     dplyr::select(Country,obsValue) %>% 
     rename(voc = obsValue)
 
-data <- merge(effects,isced,
-                 by.x = "Country")
 
-
-rm(effects)
-
-
-
-
-
-unem %>% 
-    ggplot(aes(x=reorder(COUNTRY,-obsValue),y=obsValue)) +
-      geom_col()
-
-# Merging
-data <- merge(betas_passive,unem,
-              by.x = "country")
-
-
-unemmod <- edvreg(data$b~data$yunem,
-                  omegasq = data$omega)
-summary(unemmod)
 
 
